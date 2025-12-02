f0dbea64cb3070eabaa7039ea477ba8959070e51
GeraldoSallesLobo <geraldojunior0503@gmail.com>
2025-12-02 15:52:46 -0300

    works: add process-order-details-export lambda with consolidated order items observations and invoices

---
+++ process_order_details_export/helpers/cors_headers.py
@@ -0,0 +1,19 @@
+# helpers/cors_headers.py
+ALLOWED_ORIGINS = [
+    "https://editor.weweb.io",
+    "https://sandbox.smartisupply.com.br"
+]
+
+def cors_headers(origin: str | None) -> dict:
+    headers = {
+        "Access-Control-Allow-Methods": "POST,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type,Authorization,internal-token,x-api-key"
+    }
+
+    if origin in ALLOWED_ORIGINS:
+        headers["Access-Control-Allow-Origin"] = origin
+        headers["Access-Control-Allow-Credentials"] = "true"
+        headers["Vary"] = "Origin"
+
+    return headers
+
